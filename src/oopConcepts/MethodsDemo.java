e65be94d3ed744875f5139fc518ab570c2e4afe0
Mariam <mariam.avetis@outlook.com>
2020-06-20 21:36:01 +0400

    Section 4: Classes and Methods, OOP

---
+++ src/oopConcepts/MethodsDemo.java
@@ -0,0 +1,14 @@
+package oopConcepts;
+
+public class MethodsDemo {
+
+    public static void main(String[] args) {
+        String a = "Hey there !!!";
+        System.out.println(a);
+    }
+
+    public static void method1(String[] args) {
+        String a = "Hey there !!!";
+        System.out.println(a);
+    }
+}
